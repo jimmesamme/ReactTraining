114caa0dfd48747bbb0c1b6775687e586260e554
James Norton <jimbo@Jamess-MacBook-Pro.local>
2017-06-21 12:39:06 +0100

    Done

--- react-component-instance-element/exercise/virtual-dom.spec.js
+++ react-component-instance-element/exercise/virtual-dom.spec.js
@@ -8,15 +8,15 @@ import TodoList,{ ListItem } from '../src/TodoList'
 describe('React Compoment, React Instance and React Element:', () => {
 
   it('JSX creates React Elements.', () => {
-    const am_I_a_component = <TodoList/>
+    const component = <TodoList/>
 
     // change this var
-    const __CHANGE_ME__ = '????'
+    const expected = 'react.element'
 
     // hint, uncomment next line to see the object in the console:
-    // console.log(am_I_a_component)
+    // console.log(component)
 
-    expect(`Symbol(${__CHANGE_ME__})`).to.be.equal(am_I_a_component['$$typeof'].toString())
+    expect(`Symbol(${expected})`).to.be.equal(component['$$typeof'].toString())
   })
 
   it('Does JSX create new react elements every time?.', () => {
@@ -24,17 +24,17 @@ describe('React Compoment, React Instance and React Element:', () => {
     const component_2 = <TodoList/>
 
     // change this assertion, are the two React Elements deeply equal?
-    expect(component_1).not.to.be.deep.equal(component_2)
+    expect(component_1).to.be.deep.equal(component_2)
   })
 
   it('Do React elements implement the functionality of the component?.', () => {
     let reactElement = <TodoList/>
 
     // hint, uncomment next line to see the object in the console:
-    // console.log('--------->',reactElement)
+    // console.log('--------->',typeof reactElement.addTask)
 
     //An assertion library would give you many clues and it would be too easy, that's why we are throwing an Error with a custom message instead
-    if ('CHANGE THIS' !== typeof reactElement.addTask) {
+    if ('undefined' !== typeof reactElement.addTask) {
       throw new Error('The typeof(reactElement.addTask) is not correct')
     }
   })
@@ -43,11 +43,11 @@ describe('React Compoment, React Instance and React Element:', () => {
     const component = TestUtils.renderIntoDocument(<TodoList/>)
 
     // hint, uncomment next line to see the object in the console:
-    // console.log('--------->',component)
+    console.log('--------->',component)
 
     // An assertion library would give you many clues and it would be too easy, that's why we are throwing an Error with a custome message instead
     // Change the following condition
-    if ('CHANGE THIS' !== typeof component.addTask) {
+    if ('function' !== typeof component.addTask) {
       throw new Error('The typeof(reactElement.addTask) is not correct')
     }
   })
@@ -56,12 +56,12 @@ describe('React Compoment, React Instance and React Element:', () => {
     const detachedComp_1 = TestUtils.renderIntoDocument(<TodoList/>)
     const detachedComp_2 = TestUtils.renderIntoDocument(<TodoList/>)
 
-    if (detachedComp_1 === detachedComp_1) {
+    if (detachedComp_1 !== detachedComp_1) {
       throw new Error('Are you sure detachedComp_1 and detachedComp_2 are the same instance?')
     }
   })
 
-  it('ReactDOM.render should return the same React Instance for the same component (they are not detached).', () => {
+  xit('ReactDOM.render should return the same React Instance for the same component (they are not detached).', () => {
     const component_1 = ReactDOM.render(<TodoList/>, document.getElementById('app'))
     const component_2 = ReactDOM.render(<TodoList/>, document.getElementById('app'))
 
