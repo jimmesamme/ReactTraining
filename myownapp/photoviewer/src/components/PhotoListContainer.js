dde87f5757e94b0968aeab201b7dd4d88421c25b
James Norton <jimbo@Jamess-MacBook-Pro.local>
2017-06-22 09:58:24 +0100

    Adding basic photo list container

---
+++ myownapp/photoviewer/src/components/PhotoListContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class PhotoListContainer extends Component {
+	constructor() {
+		super()
+	}
+
+	render() {
+		return <div />;
+	}
+}
+
+export default PhotoListContainer
