d576c7b0b0beb3265afdea932ac7657d07edaaf4
James Norton <jimbo@Jamess-MacBook-Pro.local>
2017-06-22 09:58:14 +0100

    Adding basic photo list container

--- myownapp/photoviewer/src/App.js
+++ myownapp/photoviewer/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 
+import PhotoListContainer from './components/PhotoListContainer.js'
+
 class App extends Component {
   render() {
     return (
       <div className="main-view">
-        
+        <PhotoListContainer />
       </div>
     );
   }
